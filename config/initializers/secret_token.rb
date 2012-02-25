d149a0762cd4ba2f47b87db54a92725031e6e455
Ryan Lovelett <ryan@wahvee.com>
2012-02-25 07:44:59 -0600

    The first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitCooler::Application.config.secret_token = '91913fb4e149ba8790630b6b81f0d420f8016e19a099f4ae383c1dd222721763fe83ad7d5e6416cf409c80ef775705492977b951f046e5dcc02554b0c097bf2c'
