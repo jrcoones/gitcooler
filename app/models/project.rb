df5fe6bcee3d1e8303ef0d8a24eefa51d2778cb3
Ryan Lovelett <ryan@wahvee.com>
2012-02-25 14:05:01 -0600

    Specs to test if a Project is created it MUST have a owner.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,17 @@ class Project < ActiveRecord::Base
 
   friendly_id :title, use: :slugged
 
+  # Validate properties
+  validates :title, :presence => true, :length => {:in => 3..255 }, :uniqueness => true,
+            :format => { :with => /^([a-z \d]+)$/i, :message => "Only letters, numbers, and spaces are allowed." }
+  validates :description, :length => { :maximum => 255 }
+
+  # The user who owns this project
+  belongs_to :owner, :class_name => "User"
+
+  # Validate that an owner is defined for the project
+  validates :owner_id, :presence => true
+
   # The users who can contribute to this project
   has_many :memberships, :dependent => :destroy
   has_many :members, :through => :memberships, :source => :user
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @current_user = Factory(:user) }
+
+  context "title" do
+    it "should only allow titles greater than 3, which only are letters, numbers and spaces" do
+      invalid_titles = ["a", "ab", "ab^", "my-name-is-ryan", "This Should Not Be Valid!"]
+      invalid_titles.each do |title|
+        expect { Factory(:project, title: title ) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    it { expect { Factory(:project, title: "This Should Be Valid" ) }.to_not raise_error(ActiveRecord::RecordInvalid) }
+    it { expect { Factory(:project, title: "This 2 Should Be Valid" ) }.to_not raise_error(ActiveRecord::RecordInvalid) }
+  end
+
+  context "owner" do
+    it { expect { Factory(:project_with_no_owner) }.to raise_error(ActiveRecord::RecordInvalid) }
+  end
 end
