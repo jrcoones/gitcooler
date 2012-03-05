6e816d3e91ea502433d3c74f3cd620b2920343d8
Ryan Lovelett <ryan@wahvee.com>
2012-03-05 11:20:03 -0600

    Now can fetch the current_users gravatar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Gravatar support
+  include Gravtastic
+  gravtastic :secure => true, :filetype => :png, :size => 48
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
