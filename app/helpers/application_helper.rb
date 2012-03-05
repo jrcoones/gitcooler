f5d576afabae6b83ddad4396903712915f1080a0
Ryan Lovelett <ryan@wahvee.com>
2012-03-05 16:05:21 -0600

    Added a navbar to the top of the page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_action
+    if user_signed_in?
+      link_to t('sign_out'), destroy_user_session_path, :method => :delete
+    else
+      link_to t('sign_in'), new_user_session_path
+    end
+  end
 end
